4ce67fcba35103bbcc4fb76e4702fa31d3ee78a4
Aisling Hemlock <aisling.hemlock@gmail.com>
2023-12-01 16:21:47 +0000

    final edits

--- README.md
+++ README.md
@@ -2,11 +2,17 @@
 
 Week 1 submission
 
+1/12/23 did not realise we had to copy the spacey website closely. i went rogue, sorry about that #adhdFail. I did ensure my website has similar features interpreted in a different way. I hope it meets the spirit of the assignment atleast, if not exactly what was asked.
+
+Foxy space(c) 2023 is a fox themed website that features multiple images of foxes. There is a floating nav bar at the top of the screen. It contains several sections with blank 'read more' buttons. It has a donate button at the bottom, and a separate button that plays a short video before actioning the donate button.
+
+I'll follow the assignment more closely next time.
+
 Scroll-behaviour inspired by @SK (30/11/23)
 html {
 scroll-behavior: smooth;
 }
-
+\*(Added scroll anchors 1/12/23)
 Added extra sections to test usability.
 
 Hyperlink function inspired by @SK (30/11/23)
@@ -26,6 +32,9 @@ z-index: 1; /_just figured out that this is important _/
 
 Logo div hyperlink guide from @SO1 (30/11/23)
 
+Added scroll snapping following the guide from @CSS1
+
 Credits:
 @SK = https://spacey-kappa.vercel.app/_/
 @SO1 = https://stackoverflow.com/questions/2188272/html-how-to-make-an-entire-div-a-hyperlink
+@CSS1 = https://css-tricks.com/practical-css-scroll-snapping/
--- script.js
+++ script.js
@@ -1,8 +1,39 @@
+//donate button//
 const button = document.getElementById("button");
 button.addEventListener("click", playVid);
 function playVid() {
-  let amount = prompt("How much would you like to donate? (GBP)");
-  if (isNaN(amount)) {
-    alert("Invalid! Please write amount in numbers only!");
-  } else alert(`Thank you for donating £${amount}`);
+  let amount = prompt("How much would you like to donate? (GBP)", 0);
+  Number(amount);
+  if (isNaN(amount) || amount == null) {
+    alert("Invalid! Please use numbers only!");
+  } else pounceBtn.textContent = `Thank you for donating £${amount}`;
 }
+
+//pounce button//
+const pounceBtn = document.getElementById("pounceBtn");
+const pounceImg = document.getElementById("contact");
+const pounceVid = document.getElementById("foxVid");
+let playing = 0;
+function noVid(playing) {
+  if (playing !== 0) {
+    pounceBtn.hidden = false;
+    pounceImg.style.backgroundImage = "url('./assets/fox-pounce.jpg')";
+    pounceImg.style.backgroundSize = "cover";
+    pounceImg.style.backgroundPosition = "center center";
+    document.getElementById("text-contact").style.color = "black";
+    foxVid.hidden = true;
+    foxVid.pause();
+    foxVid.currentTime = 0;
+    playing = 0;
+    document.getElementById("button").click();
+  }
+}
+pounceBtn.addEventListener("click", function () {
+  pounceImg.style.background = "black";
+  document.getElementById("text-contact").style.color = "white";
+  foxVid.hidden = false;
+  foxVid.play();
+  playing = 1;
+  setTimeout(noVid, 1400);
+  pounceBtn.hidden = true;
+});
